8cb0a6409b39554050bd72a6fb212778c36baa86
Christopher A. Williamson <home@chrisaw.com>
2017-07-17 17:46:49 +0100

    slightly more brute force approach to display rotation but more reliable

--- roles/xorg/files/gpdrotate.sh
+++ roles/xorg/files/gpdrotate.sh
@@ -2,8 +2,7 @@
 
 sleep 5
 
-DISPLAY=:${1}
 XAUTHORITY=$(ps aux |grep -e Xorg | head -n1 | awk '{ split($0, a, "-auth "); split(a[2], b, " "); print b[1] }')
-export DISPLAY XAUTHORITY
 
-xrandr --output DSI1 --rotate right
+DISPLAY=0 xrandr --output DSI1 --rotate right
+DISPLAY=1 xrandr --output DSI1 --rotate right
