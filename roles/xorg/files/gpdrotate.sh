5710a4c6fa9b728d51fc8640acb1864ddc102fb5
Christopher A. Williamson <home@chrisaw.com>
2017-07-17 22:55:13 +0100

    moved touchscreen rotation to gpdrotate script cleaned up dconf script filenames cleaned up xorg xsession.d script filenames

--- roles/xorg/files/gpdrotate.sh
+++ roles/xorg/files/gpdrotate.sh
@@ -4,5 +4,9 @@ sleep 5
 
 export XAUTHORITY=$(ps aux |grep -e Xorg | head -n1 | awk '{ split($0, a, "-auth "); split(a[2], b, " "); print b[1] }')
 
+# rotate display
 DISPLAY=:0 xrandr --output DSI1 --rotate right >/dev/null 2>&1
 DISPLAY=:1 xrandr --output DSI1 --rotate right >/dev/null 2>&1
+
+# rotate touchscreen
+xinput set-prop 11 "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
