8d9e5567ffe55d3ef207ab291fb1d1eceee56e29
Christopher A. Williamson <home@chrisaw.com>
2017-07-17 18:17:03 +0100

    fixed gpdrotate.sh issue

--- roles/xorg/files/gpdrotate.sh
+++ roles/xorg/files/gpdrotate.sh
@@ -2,7 +2,7 @@
 
 sleep 5
 
-XAUTHORITY=$(ps aux |grep -e Xorg | head -n1 | awk '{ split($0, a, "-auth "); split(a[2], b, " "); print b[1] }')
+export XAUTHORITY=$(ps aux |grep -e Xorg | head -n1 | awk '{ split($0, a, "-auth "); split(a[2], b, " "); print b[1] }')
 
 DISPLAY=:0 xrandr --output DSI1 --rotate right >/dev/null 2>&1
 DISPLAY=:1 xrandr --output DSI1 --rotate right >/dev/null 2>&1
