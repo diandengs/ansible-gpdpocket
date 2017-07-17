b50240c59b032a6d2ba87cb05448cef3dc13aced
Christopher A. Williamson <home@chrisaw.com>
2017-07-17 17:50:42 +0100

    added improvement to gpdrotate script

--- roles/xorg/files/gpdrotate.sh
+++ roles/xorg/files/gpdrotate.sh
@@ -4,5 +4,5 @@ sleep 5
 
 XAUTHORITY=$(ps aux |grep -e Xorg | head -n1 | awk '{ split($0, a, "-auth "); split(a[2], b, " "); print b[1] }')
 
-DISPLAY=0 xrandr --output DSI1 --rotate right
-DISPLAY=1 xrandr --output DSI1 --rotate right
+DISPLAY=:0 xrandr --output DSI1 --rotate right
+DISPLAY=:1 xrandr --output DSI1 --rotate right
