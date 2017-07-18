ad3946e4bdd585e3c4a615d35967407a5c82a6b8
Christopher A. Williamson <home@chrisaw.com>
2017-07-18 20:27:36 +0100

    resolved problem with gpdrotate starting too early

--- roles/xorg/files/gpdrotate.sh
+++ roles/xorg/files/gpdrotate.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-export XAUTHORITY=$(ps aux |grep -e Xorg | head -n1 | awk '{ split($0, a, "-auth "); split(a[2], b, " "); print b[1] }')
-export DISPLAY=${1}
+# wait for xorg to start properly
+sleep 5
+
+# determine necessary variables
+XAUTHORITY=$(ps aux |grep -e Xorg | head -n1 | awk '{ split($0, a, "-auth "); split(a[2], b, " "); print b[1] }')
+DISPLAY=${1}
+export DISPLAY XAUTHORITY
 
 # rotate display
-sleep 5
 xrandr --output DSI1 --rotate right >/dev/null 2>&1
