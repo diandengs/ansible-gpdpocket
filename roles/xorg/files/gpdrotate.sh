5814763124ae35075f0d28573a27826214c9ad24
Christopher A. Williamson <home@chrisaw.com>
2017-07-18 20:31:28 +0100

    improved gpdrotate script additionally

--- roles/xorg/files/gpdrotate.sh
+++ roles/xorg/files/gpdrotate.sh
@@ -4,9 +4,9 @@
 sleep 5
 
 # determine necessary variables
+DISPLAY=:${1}
 XAUTHORITY=$(ps aux |grep -e Xorg | head -n1 | awk '{ split($0, a, "-auth "); split(a[2], b, " "); print b[1] }')
-DISPLAY=${1}
 export DISPLAY XAUTHORITY
 
 # rotate display
-xrandr --output DSI1 --rotate right >/dev/null 2>&1
+xrandr --output DSI1 --rotate right
