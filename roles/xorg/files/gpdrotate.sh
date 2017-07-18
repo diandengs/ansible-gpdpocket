fd24d2d12c75e9a44c73fe792d719d8ecb694529
Christopher A. Williamson <home@chrisaw.com>
2017-07-18 20:24:37 +0100

    touchscreen setup vastly improved

--- roles/xorg/files/gpdrotate.sh
+++ roles/xorg/files/gpdrotate.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
 export XAUTHORITY=$(ps aux |grep -e Xorg | head -n1 | awk '{ split($0, a, "-auth "); split(a[2], b, " "); print b[1] }')
+export DISPLAY=${1}
 
 # rotate display
 sleep 5
-DISPLAY=:0 xrandr --output DSI1 --rotate right >/dev/null 2>&1
-DISPLAY=:1 xrandr --output DSI1 --rotate right >/dev/null 2>&1
-
-# rotate touchscreen
-sleep 5
-xinput set-prop 11 "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
+xrandr --output DSI1 --rotate right >/dev/null 2>&1
