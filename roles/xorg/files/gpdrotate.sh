aa0e9abfc094802e302377a70e7ab99de3d9fa07
Christopher A. Williamson <home@chrisaw.com>
2017-07-22 12:31:12 +0100

    improved DRI performance and improved gpdrotate

--- roles/xorg/files/gpdrotate.sh
+++ roles/xorg/files/gpdrotate.sh
@@ -3,30 +3,18 @@
 # wait for xorg to start properly
 sleep 5
 
-# determine necessary variables
+# set necessary variables
 DISPLAY=:${1}
+DISPLAY_OUTPUT='DSI1'
+TOUCHSCREEN_ID=$(xinput list --id-only pointer:'Goodix Capacitive TouchScreen')
 XAUTHORITY=$(ps aux |grep -e Xorg | head -n1 | awk '{ split($0, a, "-auth "); split(a[2], b, " "); print b[1] }')
 export DISPLAY XAUTHORITY
 
 # rotate display
-xrandr --output DSI1 --rotate right
+xrandr --output ${DISPLAY_OUTPUT} --rotate right
 
 # wait for rotation to complete
 sleep 5
 
 # rotate touchscreen
-SEARCH="Goodix Capacitive TouchScreen"
-
-ids=$(xinput --list | awk -v search="$SEARCH" \
-    '$0 ~ search {match($0, /id=[0-9]+/);\
-            if (RSTART) \
-              print substr($0, RSTART+3, RLENGTH-3)\
-           }'\
-     )
-
-# Rotate every instance of Touchscreen, this will workaround the ambiguity warning problem
-# that arise when calling xinput on "Goodix Capacitive Touchscreen"
-for i in $ids
-do
-    xinput set-prop $i "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
-done
+xinput set-prop ${TOUCHSCREEN_ID} 'Coordinate Transformation Matrix' 0 1 0 -1 0 1 0 0 1
